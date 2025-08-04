e66f600cefd194f736e9a70e1f7370f80f1502a3
LucyOFM7474 <osiacflorinmarian@gmail.com>
2025-08-04 17:02:46 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,36 @@
-// public/sw.js
-const CACHE = 'lucyofm-v1';
-const FILES = [
+const CACHE_NAME = 'lucyofm-cache-v1';
+const urlsToCache = [
   '/',
   '/index.html',
-  '/script.js',
   '/style.css',
+  '/script.js',
   '/manifest.json',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
   );
 });
